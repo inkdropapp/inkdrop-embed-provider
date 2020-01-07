1db949766d92d5cc853bb7d52006e6ddf50aa65b
Takuya Matsuyama <hi@craftz.dog>
2020-01-07 18:16:32 +0900

    feat(*): initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Inkdrop Embed Provider
---
+++ common.js
@@ -0,0 +1,31 @@
+var query = new URLSearchParams(window.location.search);
+var frameId = query.get('id');
+
+function sendMessage(params) {
+  window.parent.postMessage({
+    id: frameId,
+    ...params
+  },
+  'file:///');
+}
+
+document.addEventListener('click', function (event) {
+  if (event.target.tagName === 'A') {
+    event.preventDefault();
+    sendMessage({
+      message: 'embed:open-external',
+      uri: event.target.href
+    })
+  }
+}, false);
+
+const handlePageLoad = () => {
+  if (window.parent) {
+    sendMessage({
+      message: 'embed:adjust-iframe-size',
+      height: document.body.scrollHeight
+    })
+  }
+};
+
+window.addEventListener('load', handlePageLoad, false);
